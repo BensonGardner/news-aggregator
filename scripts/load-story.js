a5a8f2bf3ddee78886bcf25c33b91abb3bcf2ed5
Benson Gardner <bensongardner@gmail.com>
2017-07-16 18:19:00 -0500

    Revert "reconfig: Break load-story into separate File"
    This reverts commit 762a86ad50faf34985c3220d84872af5ff9c2584.

--- scripts/load-story.js
+++
@@ -1,9 +0,0 @@
-  function loadStory() {
-
-      console.log(storyLoadCount);
-
-      if (storyLoadCount >= stories.length)
-        return;
-
-      APP.Data.getStoryById(stories[storyLoadCount], shoveStoryIn.bind(this, key));
-  }
